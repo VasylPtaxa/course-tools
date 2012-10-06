2152f6be2b8aab6c48e93df35ae18978edc3472d
Max <maxfeldman14@gmail.com>
2012-10-06 04:26:43 -0700

    Parses both new and partially updated csvs

--- misc/login_fetcher.rb
+++ misc/login_fetcher.rb
@@ -2,21 +2,33 @@
 require 'csv'
 csv_out = File.open('new.csv', 'wb')
 oddities = File.open('odd.txt', 'wb')
+new = File.open('new.txt', 'wb')
+skip_first = true
 headers = []
 CSV.foreach('grades.csv', encoding: "UTF-8") do |row|
-  if row[4].size > 1
+  # Skip headers
+  if skip_first
+    skip_first = false
+    headers = row
     csv_out << row.join(",")
     next
   end
   csv_out << "\n"
+  # Login has already been populated
+  if not row[4].nil? and row[4].size > 3
+    csv_out << row.join(",")
+    next
+  end
   name = row[headers.index("Name")]
-  surname = name[0...name.index(",")]
-  login = `find-student #{surname} | grep ^cs169`
-  unless login["No matches for"].nil?
+  surname = name.split(",")[0]
+  login = `find-student "#{surname}" | grep "^cs169"`
+  # Student not found
+  if login =~ /No matches for/
     row[4] = ""
     csv_out << row.join(",")
     next
   end
+  # More than one student with last name
   if login.size > 10
     row[4] = ""
     csv_out << row.join(",")
@@ -27,6 +39,10 @@ CSV.foreach('grades.csv', encoding: "UTF-8") do |row|
   end
   # Actually worked and doesn't need resolution
   row[4] = login[0...-2]
+  puts "Name: #{name} just found!"
+  if (not login.nil?) and login.size > 2
+    new << "#{name}: #{login} \n"
+  end
   csv_out << row.join(",")
 end
 csv_out.close
