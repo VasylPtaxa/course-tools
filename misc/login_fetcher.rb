ffe6f738adceaf362325e70c9634293a49a8a710
Max <maxfeldman14@gmail.com>
2012-10-05 23:10:18 -0700

    Updated gitignore, added login fetcher

---
+++ misc/login_fetcher.rb
@@ -0,0 +1,35 @@
+#!usr/bin/env ruby
+require 'csv'
+csv_out = File.open('new.csv', 'wb')
+oddities = File.open('odd.txt', 'wb')
+skip_first = true
+headers = []
+CSV.foreach('grades.csv', encoding: "UTF-8") do |row|
+  if skip_first
+    skip_first = false
+    headers = row
+    csv_out << row.join(",")
+    next
+  end
+  csv_out << "\n"
+  name = row[headers.index("Name")]
+  surname = name[0...name.index(",")]
+  login = `find-student #{surname} | grep ^cs169`
+  unless login["No matches for"].nil?
+    row[4] = "dummy"
+    csv_out << row.join(",")
+    next
+  end
+  if login.size > 10
+    row[4] = "dummy"
+    csv_out << row.join(",")
+    puts "Multiple last names for: "
+    puts login
+    oddities << "#{login}, #{surname}"
+    next
+  end
+  # Actually worked and doesn't need resolution
+  row[4] = login[0...-2]
+  csv_out << row.join(",")
+end
+csv_out.close
