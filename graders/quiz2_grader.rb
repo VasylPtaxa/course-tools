ae231086205f2a78f2dabf0b7eda3913b4d33a94
Maxwell Feldman <maxwell.feldman@dhcp-130-99.eecs.berkeley.edu>
2012-10-19 14:55:47 -0700

    added quiz2 grader

---
+++ graders/quiz2_grader.rb
@@ -0,0 +1,37 @@
+#!usr/bin/env ruby
+require 'csv'
+
+# make the roster
+`make-roster quiz2 > quiz2_roster`
+
+# Do grade entering
+skip_first = true
+# construct hash of logins
+roster = {}
+File.open("quiz2_roster").each do |line|
+  roster[line[0..7]] = line
+end
+
+login_index = nil
+quiz_index = nil
+CSV.foreach('grades.csv', encoding: "UTF-8") do |row|
+  if skip_first
+    login_index = row.index "inst login"
+    quiz_index = row.index "Quiz 2"
+    skip_first = false
+    next
+  end
+  login = row[login_index]
+  if roster.has_key? login
+    score = row[quiz_index]
+    if score.nil?
+      score = "0"
+    end
+    roster[login] = roster[login][0..-2] << score << "\n"
+  end
+end
+out = File.open("quiz2", 'wb')
+roster.each_value do |val|
+  out << val
+end
+out.close
