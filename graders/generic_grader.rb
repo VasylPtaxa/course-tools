c8266eb2b80515ee76975e14dc2af13f5ef93fad
Maxwell Feldman <maxwell.feldman@dhcp-130-99.eecs.berkeley.edu>
2012-10-19 15:58:34 -0700

    Consolidated generic graders

--- graders/generic_grader.rb
+++ graders/generic_grader.rb
@@ -1,44 +1,53 @@
 #!usr/bin/env ruby
 require 'csv'
 
-puts "Enter assignment (glookup) name"
-gl_name = gets
-puts "Enter spreadsheet name"
-csv_name = gets
-# make the roster
-`make-roster #{gl_name} > #{csv_name}_roster`
-
-# Do grade entering
-skip_first = true
-# construct hash of logins
-roster = {}
-File.open("#{gl_name}_roster").each do |line|
-  roster[line[0..7]] = line
+def make_roster(gl_name, csv_name)
+  `make-roster #{gl_name} > #{gl_name}_roster`
 end
 
-name_index = nil
-login_index = nil
-
-CSV.foreach('grades.csv', encoding: "UTF-8") do |row|
-  if skip_first
-    login_index = row.index "inst login"
-    name_index = row.index csv_name
-    skip_first = false
-    next
+def grade(gl_name, csv_name)
+  # Do grade entering
+  skip_first = true
+  # construct hash of logins
+  roster = {}
+  File.open("#{gl_name}_roster").each do |line|
+    roster[line[0..7]] = line
   end
-  login = row[4] 
-  puts login
-  if roster.has_key? login
-    puts index
-    score = row[index]
-    if score.nil?
-      score = "0"
+
+  login_index = nil
+  iter_index = nil
+
+  CSV.foreach('grades.csv', encoding: "UTF-8") do |row|
+    if skip_first
+      login_index = row.index "inst login"
+      iter_index = row.index "#{csv_name}"
+      skip_first = false
+      next
     end
-    roster[login] = roster[login][0..-2] << score << "\n"
+    login = row[login_index] 
+    if roster.has_key? login
+      score = row[iter_index]
+      if score.nil?
+        score = "0"
+      end
+      roster[login] = roster[login][0..-2] << score << "\n"
+    end
   end
+  out = File.open("#{gl_name}", 'wb')
+  roster.each_value do |val|
+    out << val
+  end
+  out.close
 end
-out = File.open(gl_name, 'wb')
-roster.each_value do |val|
-  out << val
+
+def main(gl_name, csv_name)
+  make_roster(gl_name, csv_name)
+  grade(gl_name, csv_name)
 end
-out.close
+
+if __FILE__ == $0
+  # Get the assignment name for glookup, grades.csv (Respectively)
+  gl_name = ARGV[0]
+  csv_name = ARGV[1]
+  main(gl_name, csv_name)
+end
